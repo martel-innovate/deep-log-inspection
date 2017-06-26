59ea3f7f1e62df6486ca9efa51b8419b30638fcd
Michele Chersich <michele.chersich@gmail.com>
2017-06-26 18:45:16 +0200

    certbot.sh updated

--- log-server/scripts/certbot.sh
+++ log-server/scripts/certbot.sh
@@ -1,1 +1,9 @@
-docker run --rm certbot/certbot certbot certonly --standalone --preferred-challenges tls-sni -d example.com
+cd ~/deep-log-inspection/log-server/certbot
+docker run --rm -p 80:80 -p 443:443 \
+    -v "$PWD/etc/letsencrypt:/etc/letsencrypt" \
+    -v "$PWD/var/lib/letsencrypt:/var/lib/letsencrypt" \
+    -v "$PWD/var/log/letsencrypt:/var/log/letsencrypt" \
+    certbot/certbot certonly --standalone --non-interactive --agree-tos \
+        --email michele.chersich@martel-innovate.com \
+        --preferred-challenges http \
+        -d fiware-lab-211.cloudlab.zhaw.ch
