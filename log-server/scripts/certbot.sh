63c919d2b34e7d8ba1b682e14878ab940ef3d783
Michele Chersich <michele.chersich@gmail.com>
2017-06-27 21:58:25 +0200

    certbot.sh: container name and domain to be set

--- log-server/scripts/certbot.sh
+++ log-server/scripts/certbot.sh
@@ -3,11 +3,11 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel) && \
 cd $PROJECT_ROOT/log-server && mkdir -p certbot
 cd certbot
 
-docker run --rm -p 80:80 -p 443:443 \
+docker run --rm -p 80:80 -p 443:443 --name certbot \
     -v "$PWD/etc/letsencrypt:/etc/letsencrypt" \
     -v "$PWD/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "$PWD/var/log/letsencrypt:/var/log/letsencrypt" \
     certbot/certbot certonly --standalone --non-interactive --agree-tos \
         --email michele.chersich@martel-innovate.com \
         --preferred-challenges http \
-        -d fiware-lab-211.cloudlab.zhaw.ch
+        -d myDNSname.com
