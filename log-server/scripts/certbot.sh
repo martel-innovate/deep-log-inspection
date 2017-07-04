796ddd2b1d4722487866356599328660d01a1b60
Michele Chersich <michele.chersich@gmail.com>
2017-07-04 17:12:06 +0200

    certbot: added flag for automatic renewal

--- log-server/scripts/certbot.sh
+++ log-server/scripts/certbot.sh
@@ -6,7 +6,11 @@ docker run --rm -p 80:80 -p 443:443 --name certbot \
     -v "$PWD/etc/letsencrypt:/etc/letsencrypt" \
     -v "$PWD/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "$PWD/var/log/letsencrypt:/var/log/letsencrypt" \
-    certbot/certbot certonly --standalone --non-interactive --agree-tos \
+    certbot/certbot certonly \
+        --standalone \
+        --non-interactive \
+        --agree-tos \
+        --renew-by-default \
         --email michele.chersich@martel-innovate.com \
         --preferred-challenges http \
-        -d myDNSname.com
+        --domain myDNSname.com
