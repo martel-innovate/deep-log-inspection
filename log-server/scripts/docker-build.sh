b9c841b97edbff3630186df695c5972e8fa0ab93
Michele Chersich <michele.chersich@gmail.com>
2017-07-13 13:22:28 +0200

    updated docker-build.sh to pull docker images

--- log-server/scripts/docker-build.sh
+++ log-server/scripts/docker-build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd $(dirname $0) && cd .. && \
 
-docker build -t monasca-log-api monasca-log-api
-docker build -t log-transformer log-transformer
-docker build -t log-persister log-persister
+docker build -t monasca-log-api monasca-log-api && \
+docker build -t log-transformer log-transformer && \
+docker build -t log-persister log-persister && \
+
+docker pull zookeeper:3.3 && \
+docker pull monasca/kafka:0.9.0.1-2.11 && \
+docker pull docker.elastic.co/elasticsearch/elasticsearch:5.4.0 && \
+docker pull docker.elastic.co/kibana/kibana:5.4.0 && \
+
+docker pull mysql:5.5 && \
+docker pull monasca/mysql-init:1.0.0 && \
+docker pull monasca/keystone:1.0.6
