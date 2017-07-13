224a8c1fd0cc71a2802e66c4a6eea963b2146002
Michele Chersich <michele.chersich@gmail.com>
2017-07-13 12:37:30 +0200

    replaced localhost with 127.0.0.1 in bash scripts for Elasticsearch

--- log-server/scripts/es-delete-templates.sh
+++ log-server/scripts/es-delete-templates.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-curl -XDELETE localhost:9200/_template/os
+curl -XDELETE 127.0.0.1:9200/_template/os
 
-curl -XDELETE localhost:9200/_template/os-nova-api-osapi_compute
-curl -XDELETE localhost:9200/_template/os-nova-api-metadata
-curl -XDELETE localhost:9200/_template/os-nova-compute
+curl -XDELETE 127.0.0.1:9200/_template/os-nova-api-osapi_compute
+curl -XDELETE 127.0.0.1:9200/_template/os-nova-api-metadata
+curl -XDELETE 127.0.0.1:9200/_template/os-nova-compute
 
-curl -XDELETE localhost:9200/_template/os-neutron-server
+curl -XDELETE 127.0.0.1:9200/_template/os-neutron-server
 
-curl -XDELETE localhost:9200/_template/os-cinder-api
+curl -XDELETE 127.0.0.1:9200/_template/os-cinder-api
 
-curl -XDELETE localhost:9200/_template/os-keystone-api
+curl -XDELETE 127.0.0.1:9200/_template/os-keystone-api
