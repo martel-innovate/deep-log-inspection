73576d945dfaa956859fdf4ee824033cf29b3cd6
Michele Chersich <michele.chersich@gmail.com>
2017-07-19 15:26:10 +0200

    set up basic X-Pack authentication: user and password

--- log-server/scripts/es-delete-templates.sh
+++ log-server/scripts/es-delete-templates.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-curl -XDELETE 127.0.0.1:9200/_template/os
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os
 
-curl -XDELETE 127.0.0.1:9200/_template/os-nova-api-osapi_compute
-curl -XDELETE 127.0.0.1:9200/_template/os-nova-api-metadata
-curl -XDELETE 127.0.0.1:9200/_template/os-nova-compute
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-osapi_compute
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-metadata
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-nova-compute
 
-curl -XDELETE 127.0.0.1:9200/_template/os-neutron-server
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-neutron-server
 
-curl -XDELETE 127.0.0.1:9200/_template/os-cinder-api
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-cinder-api
 
-curl -XDELETE 127.0.0.1:9200/_template/os-keystone-api
+curl -XDELETE elastic:martellab17@127.0.0.1:9200/_template/os-keystone-api
--- log-server/scripts/es-push-templates.sh
+++ log-server/scripts/es-push-templates.sh
@@ -2,29 +2,29 @@
 cd $(dirname $0) && cd ../templates
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os \
     --data @os.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-nova-api-osapi_compute \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-osapi_compute \
     --data @os-nova-api-osapi_compute.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-nova-api-metadata \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-metadata \
     --data @os-nova-api-metadata.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-nova-compute \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-compute \
     --data @os-nova-compute.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-neutron-server \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-neutron-server \
     --data @os-neutron-server.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-cinder-api \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-cinder-api \
     --data @os-cinder-api.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT 127.0.0.1:9200/_template/os-keystone-api \
+    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-keystone-api \
     --data @os-keystone-api.json
