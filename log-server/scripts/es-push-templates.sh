dba9ecaa198bd7fbdceb6efb99a548ce03d73bf6
Michele Chersich <michele.chersich@gmail.com>
2017-07-20 13:35:07 +0200

    pass credentials to Logstash and Kibana config files via environment variables

--- log-server/scripts/es-push-templates.sh
+++ log-server/scripts/es-push-templates.sh
@@ -2,29 +2,29 @@
 cd $(dirname $0) && cd ../templates
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os \
     --data @os.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-osapi_compute \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-api-osapi_compute \
     --data @os-nova-api-osapi_compute.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-api-metadata \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-api-metadata \
     --data @os-nova-api-metadata.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-nova-compute \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-compute \
     --data @os-nova-compute.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-neutron-server \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-neutron-server \
     --data @os-neutron-server.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-cinder-api \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-cinder-api \
     --data @os-cinder-api.json && \
 
 curl -H "Content-Type: application/json" \
-    -XPUT elastic:martellab17@127.0.0.1:9200/_template/os-keystone-api \
+    -XPUT $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-keystone-api \
     --data @os-keystone-api.json
