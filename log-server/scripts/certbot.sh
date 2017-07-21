302387b5a6726d31ef0b5c3592e791e3f229083f
Michele Chersich <michele.chersich@gmail.com>
2017-07-21 17:39:27 +0200

    SSL certificate issued for Monasca Log API; Monasca Log API now runs on port 443, Kibana on port 80

--- log-server/scripts/certbot.sh
+++ log-server/scripts/certbot.sh
@@ -1,5 +1,6 @@
-
-docker run --rm -p 80:80 -p 443:443 --name certbot \
+#!/bin/bash
+# request SSL certificate
+docker run --rm -p 443:443 --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/var/log/letsencrypt:/var/log/letsencrypt" \
@@ -9,5 +10,10 @@ docker run --rm -p 80:80 -p 443:443 --name certbot \
         --agree-tos \
         --renew-by-default \
         --email michele.chersich@martel-innovate.com \
-        --preferred-challenges http \
-        --domain deeplogmanager.lab.fiware.org
+        --preferred-challenges tls-sni \
+        --domain deeplogmanager.lab.fiware.org && \
+
+# automate certificate renewal
+cd $(dirname $0) && \
+cp certbot-renew /etc/cron.daily/ && \
+chmod 755 /etc/cron.daily/certbot-renew
