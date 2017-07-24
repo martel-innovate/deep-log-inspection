c31da53ae0f3a55ddcf3d45ad4e9c980005fe425
Michele Chersich <michele.chersich@gmail.com>
2017-07-24 15:35:03 +0200

    restored port 8090 for Monasca Log API; changed certicate renewal: port 80, interval from daily to weekly

--- log-server/scripts/certbot.sh
+++ log-server/scripts/certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # request SSL certificate
-docker run --rm -p 443:443 --name certbot \
+docker run --rm -p 80:80 --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/var/log/letsencrypt:/var/log/letsencrypt" \
@@ -10,10 +10,10 @@ docker run --rm -p 443:443 --name certbot \
         --agree-tos \
         --renew-by-default \
         --email michele.chersich@martel-innovate.com \
-        --preferred-challenges tls-sni \
+        --preferred-challenges http \
         --domain deeplogmanager.lab.fiware.org && \
 
 # automate certificate renewal
 cd $(dirname $0) && \
-cp certbot-renew /etc/cron.daily/ && \
-chmod 755 /etc/cron.daily/certbot-renew
+cp certbot-renew /etc/cron.weekly/ && \
+chmod 755 /etc/cron.weekly/certbot-renew
