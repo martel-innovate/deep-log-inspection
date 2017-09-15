1b8826b71a0917a7bf3099b0631cc3d2f1ac618c
Michele Chersich <michele.chersich@gmail.com>
2017-09-15 11:52:39 +0200

    Dev: elasticsearch cluster and traefik reverse proxy (#13)
    * deleted Elasticsearch and Kibana configuration files and added settings in compose file
    
    * updated compose files
    
    * changed Elasticsearch image to martel/docker-swarm-elasticsearch
    
    * deployed elasticsearch to 3-node cluster, disabled x-pack security
    
    * removed ulimits from elasticsearch in compose file
    
    * deployed elasticsearch cluster: system now up and operational
    
    * installed certbot and updated scripts
    
    * removed authentication to Elasticsearch from Logstash pipeline
    
    * removed SSL certificate settings as SSL is managed by traefik
    
    * basic http traefik setup (no TLS)
    
    * deleted overlay network 'frontend'
    
    * set up letsencrypt certificate inside traefik
    
    * updated README
    
    * traefik now reverse-proxies elasticsearch too
    
    * traefik rule PathPrefixStrip does not work correctly
    
    * replaced traefik path rules with host rules
    
    * modified entrypoints to grant TLS to all backends
    
    * certbot client is no more necessary
    
    * simplified directory path in script
    
    * disabled xpack monitoring
    
    * removed docker-compose restart policy as system is supposed to run in swarm mode
    
    * removed logstash persister x-pack authentication environment variables
    
    * included missing Monasca Log Agent's Dockerfile

--- log-server/scripts/es-delete-templates.sh
+++ log-server/scripts/es-delete-templates.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os
 
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-api-osapi_compute
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-api-metadata
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-nova-compute
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-nova-api-osapi_compute
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-nova-api-metadata
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-nova-compute
 
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-neutron-server
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-neutron-server
 
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-cinder-api
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-cinder-api
 
-curl -XDELETE $ELASTIC_USER:$ELASTIC_PW@127.0.0.1:9200/_template/os-keystone-api
+curl -XDELETE elastic.${DOMAIN:-localhost}/_template/os-keystone-api
