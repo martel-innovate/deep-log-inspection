b1e4a3e77320cb4557507c4c7237faefdcfaed85
chersicm <michele.chersich@gmail.com>
2017-10-31 00:31:45 +0100

    certificates and basic configuration (TODO: sgadmin.sh gets killed)

--- log-server/elasticsearch/bin/init_sg.sh
+++ log-server/elasticsearch/bin/init_sg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-plugins/search-guard-5/tools/sgadmin.sh -cd config/ -ts config/truststore.jks -ks config/kirk-keystore.jks -nhnv -cn deeplog
+plugins/search-guard-5/tools/sgadmin.sh -cd config/ -ts config/truststore.jks -tspass $TS_PASS -ks config/sgadmin-keystore.jks -kspass $KS_PASS -nhnv -cn deeplog
